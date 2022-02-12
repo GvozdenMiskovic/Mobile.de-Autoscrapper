12ca2361b3718a5f40159f3808f6602f9dcf6188
Gvozden Miskovic <gvozdje@gmail.com>
2022-02-12 17:01:09 +0100

    update scrpe boundary

--- src/ScrapeRequester/Scrape.boundary.ts
+++ src/ScrapeRequester/Scrape.boundary.ts
@@ -1,1 +1,8 @@
-export type ScrapeResponse = {}
+export type ScrapeResponse = {
+    title: string;
+    mainPictureUrl: string;
+    table: Map<string, string[]>;
+    description?: string;
+    price: number;
+    otherImageUrls: string[];
+}
