e605cf8ade75041702221a32a6ee62a753d00725
Gvozden Miskovic <gvozdje@gmail.com>
2022-02-12 16:26:12 +0100

    update scrape controller

--- src/api/ScrapeController.ts
+++ src/api/ScrapeController.ts
@@ -1,8 +1,15 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { ScrapeResponse } from "../ScrapeRequester/Scrape.boundary";
+import { ScrapeRequesterFactory } from "../ScrapeRequester/ScrapeRequesterFactory";
 
 @Controller("/scrape")
 export class ScrapeController {
+    private requesterFactory: ScrapeRequesterFactory;
+
+    constructor(requesterFactory: ScrapeRequesterFactory) {
+        this.requesterFactory = requesterFactory;
+    }
+
     @Get()
     public async getRawLinkData(@Param('link') link: string): Promise<ScrapeResponse> {
         return undefined;
