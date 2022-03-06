d2ef435cd94269a94c4bb101133519de8f2d7d41
Gvozden Miskovic <gvozdje@gmail.com>
2022-03-06 18:17:46 +0100

    add pdf kit

--- src/app/PdfGenerator/PdfBuilder/PdfBuilderInterface.ts
+++ src/app/PdfGenerator/PdfBuilder/PdfBuilderInterface.ts
@@ -2,5 +2,6 @@ export interface PdfBuilderInterface {
     addTitlePage(title: string, pathToMainImage: string): PdfBuilderInterface;
     addImageGridPages(imagePaths: string[]): PdfBuilderInterface;
     addDescriptionPages(tableInformation: Map<string, string>, description: string): PdfBuilderInterface;
-    build(): string;
+    build(): Promise<string>;
 }
+
