8a0993af049a096c05e14f5d6d1c2e4d4cd4dfee
Cyprien Eckert <cipri.eckert@hotmail.fr>
2022-03-20 20:41:22 +0100

    add serving of frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { PdfCreationModule } from "./api/PdfCreation/PdfCreation.module";
+import { ServeStaticModule } from "@nestjs/serve-static"
+import { join } from "path";
 
 @Module({
     imports: [
-        PdfCreationModule
+        PdfCreationModule,
+        ServeStaticModule.forRoot({
+          rootPath: join(__dirname, '../src', 'client'),
+          exclude: ['/api*'],
+        }),
     ]
 })
 export class AppModule {}
